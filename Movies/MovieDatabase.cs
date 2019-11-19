5bd95b870c518993507cc5f239ab4e732c2cc63b
etnguyen84 <etnguyen84@E1117W08>
2019-11-19 15:16:16 -0600

    added search and filter functionality

--- Movies/MovieDatabase.cs
+++ Movies/MovieDatabase.cs
@@ -27,5 +27,48 @@ namespace Movies
         }
 
         public List<Movie> All { get { return movies; } }
+
+        public List<Movie> Search(string term)
+        {
+            List<Movie> results = new List<Movie>();
+
+            foreach(Movie m in movies)
+            {
+                if(m.Title.Contains(term, StringComparison.OrdinalIgnoreCase) || (m.Director != null && m.Director.Contains(term, StringComparison.OrdinalIgnoreCase)))
+                {
+                    results.Add(m);
+                }
+            }
+
+            return results;
+        }
+
+        public List<Movie> FilterByMPAA(List<Movie> movies, List<string> mpaa)
+        {
+            List<Movie> results = new List<Movie>();
+
+            foreach (Movie m in movies)
+            {
+                if (mpaa.Contains(m.MPAA_Rating))
+                {
+                    results.Add(m);
+                }
+            }
+            return results;
+        }
+
+        public List<Movie> FilterByMinIMDB(List<Movie> movies, float min)
+        {
+            List<Movie> results = new List<Movie>();
+
+            foreach (Movie m in movies)
+            {
+               if(m.IMDB_Rating != null && m.IMDB_Rating >= min)
+                {
+                    results.Add(m);
+                }
+            }
+            return results;
+        }
     }
 }
