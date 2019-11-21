1904eb6784a04c415ee02170ce61f3067429ff21
etnguyen84 <etnguyen84@E1117W08>
2019-11-21 15:25:13 -0600

    ajax stuff

--- Movies/wwwroot/js/site.js
+++ Movies/wwwroot/js/site.js
@@ -2,3 +2,33 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+document.getElementById("search-form");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var inputs = document.getElementByName("input");
+    var formData = new FormData();
+    var token = "";
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].type == 'checkbox') {
+            if(inputs[i].checked) formData.append(inputs[i].name, inputs[i].value);
+        }
+        else {
+            formData.set(inputs[i].name, inputs[i].value);
+        }
+        if (inputs[i].name == "__RequestVerificationToken") {
+            token = inputs[i].value;
+        }
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', '/', true);
+    xhr.setRequestHeader("RequestVerificationToken", token)
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status == 200) {
+            console.log(xhr.responseText);
+        }
+    }
+    xhr.send(formData);
+});
